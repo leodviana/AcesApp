d875dd9d024e8a2237ba062d3926ec2b1ca4a13e
Leonardo Viana <damasceno.leonardo@gmail.com>
2021-03-05 17:48:11 -0300

    inclusao das fontes

--- Aces/AcesApp/AcesApp/Properties/AssemblyInfo.cs
+++ Aces/AcesApp/AcesApp/Properties/AssemblyInfo.cs
@@ -1,3 +1,17 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Roboto-Black.ttf", Alias = "FontRobotoBlack")]
+[assembly: ExportFont("Roboto - BlackItalic.ttf", Alias = "FontRobotoBlackItalic")]
+[assembly: ExportFont("Roboto-Bold.ttf", Alias = "FontRobotoBold")]
+[assembly: ExportFont("Roboto-BoldItalic.ttf", Alias = "FontRobotoBolditalic")]
+[assembly: ExportFont("Roboto-Light.ttf", Alias = "FontRobotoLight")]
+[assembly: ExportFont("Roboto-LightItalic.ttf", Alias = "FontRobotoLightitalic")]
+[assembly: ExportFont("Roboto-Medium.ttf", Alias = "FontRobotomedium")]
+[assembly: ExportFont("Roboto-MediumItalic.ttf", Alias = "FontRobotoMediumitalic")]
+[assembly: ExportFont("Roboto-Regular.ttf", Alias = "FontRobotoRegular")]
+[assembly: ExportFont("Roboto-Thin.ttf", Alias = "FontRobotothin")]
+[assembly: ExportFont("Roboto-ThinItalic.ttf", Alias = "FontRobotothintalic")]
+
+
