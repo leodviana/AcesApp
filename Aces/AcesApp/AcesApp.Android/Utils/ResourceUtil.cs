d4d6d3c01f1d74a59841204b0ba9d1e5dc0b0e05
Leonardo Viana <damasceno.leonardo@gmail.com>
2021-03-05 09:56:41 -0300

    custom renderer tabpaged android

---
+++ Aces/AcesApp/AcesApp.Android/Utils/ResourceUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Android.Content;
+
+namespace AcesApp.Droid.Utils
+{
+    public class ResourceUtil
+    {
+        public static int GetDrawableIdByFileName(string fileName, Context context)
+        {
+            return context.Resources.GetIdentifier(fileName, "drawable", context.PackageName);
+        }
+    }
+}
